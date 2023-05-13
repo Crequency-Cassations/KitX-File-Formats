15fe7277d365cda412721f750a40c87ec6c73a72
Dynesshely <catrol@qq.com>
2023-05-13 15:00:43 +0800

    💾 Feat(KitX.KXP.Helper): Use byte array instead of `u8` for cross-platform competitive.

--- KitX.KXP.Helper/Header.cs
+++ KitX.KXP.Helper/Header.cs
@@ -2,7 +2,27 @@
 
 internal static class Header
 {
-    public static byte[] header = "It is a KXP file"u8.ToArray();
+    public static byte[] header = new byte[16]
+    {
+            073, // I
+            116, // t
+            032, //  
+            105, // i
+            115, // s
+            032, //  
+            097, // a
+            032, //  
+            075, // K
+            088, // X
+            080, // P
+            032, //  
+            102, // f
+            105, // i
+            108, // l
+            101, // e
+
+        // It is a KXP file
+    };
 
     /// <summary>
     /// 验证文件是否为 KXP 文件
